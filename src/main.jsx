c59922cf9d532e08f4123f393e52269ad8aa46eb
kevinarsankamto05 <201110097@student.mercubuana-yogya.ac.id>
2024-07-02 12:03:02 +0700

    update varcel-1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./dist/css/main.css";
+
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
